db38639f2ef877a3169ef989d71513aed08925cd
kiyo <m.kiyo.may@gmail.com>
2015-07-12 11:01:07 +0900

    位置情報を取得するサンプル

--- himayanen/app/src/main/java/jp/osaka/himayanen/himayanen/MainActivity.java
+++ himayanen/app/src/main/java/jp/osaka/himayanen/himayanen/MainActivity.java
@@ -1,17 +1,86 @@
 package jp.osaka.himayanen.himayanen;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+    private LocationManager mLocationManager;
+    private LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            //Toast.makeText(MainActivity.this, location.toString(), Toast.LENGTH_SHORT).show();
+            Log.d("Himayanen-Location", location.toString());
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            switch (status) {
+                case LocationProvider.AVAILABLE:
+                    Toast.makeText(MainActivity.this, "AVAILABLE", Toast.LENGTH_SHORT).show();
+                    break;
+                case LocationProvider.OUT_OF_SERVICE:
+                    Toast.makeText(MainActivity.this, "OUT_OF_SERVICE", Toast.LENGTH_SHORT).show();
+                    break;
+                case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                    Toast.makeText(MainActivity.this, "TEMPORARY_UNAVAILABLE", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Toast.makeText(MainActivity.this, "onProviderEnabled", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Toast.makeText(MainActivity.this, "onProviderDisabled", Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // ロケーションマネージャのインスタンスを取得する
+        mLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        // 位置情報の更新を受け取るように設定
+        mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, // プロバイダ
+                100, // 通知のための最小時間間隔
+                0, // 通知のための最小距離間隔
+                mLocationListener); // 位置情報リスナー
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mLocationManager.removeUpdates(mLocationListener);
     }
 
     @Override
